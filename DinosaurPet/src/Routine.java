63eaaa8e12a4d8f663476fd36c03cb5f50ba76e3
Travis Crabtree <48073815+qu7@users.noreply.github.com>
2019-11-09 09:15:12 -0600

    Loops, mood updates, hunger updates

--- DinosaurPet/src/Routine.java
+++ DinosaurPet/src/Routine.java
@@ -2,26 +2,39 @@ import java.util.Scanner;
 
 public class Routine {
 	Scanner scan = new Scanner(System.in);
+	Dinosaur tri = new Dinosaur(0, 50, 50, "Triceratops");
+	boolean life = true;
 	
 	public Routine() {
-		Dinosaur tri = new Dinosaur(0, 50, 50, "Triceratops");
+	do {
 		System.out.println(tri.name + " is looking around");
-		System.out.println("What do you want to do? 1. Check \n2. Feed");
+		System.out.println("What do you want to do? \n1. Check \n2. Feed");
 		int command = scan.nextInt();
 		if (command == 1) {
 			System.out.println(tri.toString());
 			if (tri.happy >= 75) {
 				System.out.println(tri.name + " is in a good mood!");
 			}
+			
 			else if (tri.happy < 75 & tri.happy >= 45) {
 				System.out.println(tri.name + " is doing alright.");
 			}
+			
 			else {
 				System.out.println(tri.name + " is mad!");
 			}
+
 		}
-		else {
+		else if (command == 2)	{
 			System.out.println(tri.name + " likes this food!");
+			tri.happy += 5;
+			tri.hunger -= 5;
 		}
+		
+		else {
+			System.out.println("That's not one of the options!");
 		}
+		
+		} while (life == true);
+	}
 }
