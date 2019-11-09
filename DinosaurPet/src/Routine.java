ea1acbfe019eb2f5d4a883126aa8d88a37a516fa
Travis Crabtree <48073815+qu7@users.noreply.github.com>
2019-11-09 09:27:14 -0600

    Update Routine.java

--- DinosaurPet/src/Routine.java
+++ DinosaurPet/src/Routine.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Routine {
 	Scanner scan = new Scanner(System.in);
 	Dinosaur tri = new Dinosaur(0, 50, 50, "Triceratops");
+	int food = 3;
 	boolean life = true;
 	
 	public Routine() {
 	do {
 		System.out.println(tri.name + " is looking around");
-		System.out.println("What do you want to do? \n1. Check \n2. Feed");
+		System.out.println("What do you want to do? \n1. Status \n2. Feed");
 		int command = scan.nextInt();
 		if (command == 1) {
 			System.out.println(tri.toString());
@@ -23,12 +24,14 @@ public class Routine {
 			else {
 				System.out.println(tri.name + " is mad!");
 			}
+			System.out.println("Food: " + food);
 
 		}
 		else if (command == 2)	{
 			System.out.println(tri.name + " likes this food!");
 			tri.happy += 5;
 			tri.hunger -= 5;
+			food -= 1;
 		}
 		
 		else {
