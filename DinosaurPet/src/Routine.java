857706ec86e4712f004912964def08246e02ecab
Travis Crabtree <48073815+qu7@users.noreply.github.com>
2019-11-13 17:31:40 -0600

    Update Routine.java

--- DinosaurPet/src/Routine.java
+++ DinosaurPet/src/Routine.java
@@ -5,11 +5,12 @@ public class Routine {
 	Scanner scan = new Scanner(System.in);
 	Dinosaur tri = new Dinosaur(0, 50, 50, "Triceratops");
 	int food = 3;
+	int gold = 0;
 	boolean life = true;
 	Random r = new Random();
-	int rand2;
-	int rand3;
-	int rand4;
+	int rand2;			// 0, 1
+	int rand3; 			// 0, 1, 2
+	int rand4; 			// 0, 1, 2, 3
 	
 	public Routine() {
 	do {
@@ -35,13 +36,14 @@ public class Routine {
 				System.out.println(tri.name + " is mad!");
 			}
 			System.out.println("Food: " + food);
+			System.out.println("Gold: " + gold);
 
 		}
 		
-		// command 2 feeds the dinosaur
+		// command 2 feeds the pet
 		else if (command == 2)	{
 			System.out.println(tri.name + " likes this food!");
-			tri.happy += 5;
+			tri.happy += 2;
 			tri.hunger -= 5;
 			food = food - 1;
 		}
@@ -52,6 +54,8 @@ public class Routine {
 			System.out.println(tri.name + " is exploring the area.");
 			if (tri.happy >= 80) {
 				System.out.println("He looks pretty confident!");
+				
+				// from 0 to 3, if rand4 == 1, 2 or 3
 				if (rand4 >= 1) {
 					System.out.println("Success! He found a lot of food!");
 					food += 3;
@@ -67,6 +71,8 @@ public class Routine {
 			
 			else if (tri.happy >= 30) {
 				System.out.println("Looking... looking...");
+				
+				// from 0 to 2, if rand3 equals 1 or 2
 				if (rand3 >= 1) {
 					System.out.println("Alright! He found  some food!");
 					food += 1;
@@ -81,6 +87,8 @@ public class Routine {
 			
 			else {
 				System.out.println("Doesn't look like he's too motivated.");
+				
+				// from 0 to 1, if rand2 equals 1
 				if (rand2 == 1) {
 					System.out.println("Good! He found  some food!");
 					food += 1;
